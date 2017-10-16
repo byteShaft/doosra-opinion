21bc398278061b23076c9bf175299d6c3296b1d3
Shahid Ghafoor <shgr8rb@gmail.com>
2017-10-16 13:56:17 +0500

    remove login from launcher

--- app/src/main/java/com/byteshaft/doosra/accounts/AccountManager.java
+++ app/src/main/java/com/byteshaft/doosra/accounts/AccountManager.java
@@ -1,6 +1,5 @@
 package com.byteshaft.doosra.accounts;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,9 +7,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.byteshaft.doosra.MainActivity;
 import com.byteshaft.doosra.R;
-import com.byteshaft.doosra.utils.AppGlobals;
 
 public class AccountManager extends AppCompatActivity {
 
@@ -23,12 +20,14 @@ public class AccountManager extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (AppGlobals.isLogin()) {
-            startActivity(new Intent(AccountManager.this, MainActivity.class));
-        } else {
-            setContentView(R.layout.activity_account_manager);
-            loadLoginFragment(new Login());
-        }
+//        if (AppGlobals.isLogin()) {
+//            startActivity(new Intent(AccountManager.this, MainActivity.class));
+//        } else {
+        // setcontentview
+//            loadLoginFragment(new Login());
+//        }
+
+        setContentView(R.layout.activity_account_manager);
         sInstance = this;
     }
 
